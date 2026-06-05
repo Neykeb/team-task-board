5be03cbbf3856cce6cf6c616243a7da37ed162d3
Neykeb <mamiri4525@gmail.com>
2026-06-05 15:17:51 +0200

    feat: update Header component to display total and completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
- 
-import './App.css'
-import { Header } from './components/Header/header'
+import "./App.css";
+import { Header } from "./components/Header/header";
 
 function App() {
+  const tasks = [
+    { id: 1, status: "erledigt" },
+    { id: 2, status: "offen" },
+    { id: 3, status: "erledigt" },
+  ];
+
+  const totalTasks = tasks.length;
+
+  const doneTasks = tasks.filter((task) => task.status === "erledigt").length;
 
   return (
-    <>
-      <Header />
-    </>
-  )
+    <main className="max-w-5xl mx-auto p-6">
+      <Header totalTasks={totalTasks} doneTasks={doneTasks} />
+
+      {/* Neue Aufgaben hinzufpgen */}
+      {/* .... */}
+      
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,28 +1,21 @@
-export function Header(){
+export function Header({ totalTasks, doneTasks }) {
+  return (
+    <header className="bg-gray-100 rounded-2xl p-8 shadow-sm border border-gray-100 ">
+      <h1 className="text-4xl font-bold text-gray-900">Team Task Board</h1>
 
-    const tasks = 8
-    const doneTasks = 2
+      <p className="mt-2 text-gray-500">Verwalte Aufgaben für dein Team.</p>
 
-    return (
-      <>
-        <header className="">
+      <div className="flex gap-6 mt-6 text-sm">
+        <div className="bg-gray-100 px-4 py-2 rounded-xl">
+          Aufgaben: <span className="font-semibold">{totalTasks}</span>
+        </div>
 
-          <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
-            <img
-              src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
-              alt="Paris"
-              className="absolute inset-0 h-full w-full object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-900/40"></div>
-            <h3 className="z-10 mt-3  text-2xl font-bold text-white">
-              Team Task Board
-              <p className="text-xl font-normal mt-3">Aufgaben: {tasks}</p>
-            </h3>
-            <h3 className="z-10 mt-3 text-2xl font-bold text-white">
-              <p className="text-xl font-normal">Erleigte Aufgaben: {doneTasks}</p>
-            </h3>
-          </article>
-        </header>
-      </>
-    );
+        <div className="bg-green-100 text-green-700 px-4 py-2 rounded-xl">
+          Erledigt: <span className="font-semibold">{doneTasks}</span>
+        </div>
+      </div>
+    </header>
+  );
 }
+
+ 
